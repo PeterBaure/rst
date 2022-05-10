467d6f9aafdc5d069e33751f6012fafb15d48690
PeterBaure <100369690+PeterBaure@users.noreply.github.com>
2022-05-10 10:46:12 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let distance = 0
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+    basic.pause(200)
+})
